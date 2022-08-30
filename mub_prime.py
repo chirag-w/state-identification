f982d49155db91c2f4d6dd59f010f1d677d74ba0
chirag-w <emose@DESKTOP-R95NK6U.localdomain>
2022-08-30 16:28:51 +0530

    General prime dimension MUB

---
+++ mub_prime.py
@@ -0,0 +1,36 @@
+import numpy as np
+from util import *
+from sdp import *
+
+def mub_prime_states(p):
+    #Returns all states associated with p-dimensional MUBs where p is prime
+    Z = np.diag(np.exp(2j*np.pi * np.arange(p)/p))
+    X = np.zeros((p,p), dtype = complex)
+    for i in range(p):
+        X[(i+1)%p][i] = 1
+    M = [Z]
+    prod = X.copy()
+    for i in range(p):
+        M.append(prod)
+        prod = prod@Z
+
+    #States are eigenvectors of each matrix in M
+    S = []
+    for i in range(len(M)):
+        _,v = np.linalg.eig(M[i])
+        for col in range(p):
+            S.append(v[:,col])
+    return density_matrix(S)
+
+
+S = mub_prime_states(3)
+for i in range(len(S)):
+    print(S[i])
+
+for n in range(1,4):
+    print(n, "copies:")
+    p,M = state_identification(S,n)
+    print("State identification probability: ",p)
+    print("Optimal measurement operators: ")
+    for i in range(len(M)):
+        print(M[i])
